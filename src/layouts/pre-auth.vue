1db3393cd277341508eaa635af1f72488a688c1c
Andrei Vintila <vintila.andrei@gmail.com>
2022-03-04 17:48:13 +0200

    header redesign

--- src/layouts/pre-auth.vue
+++ src/layouts/pre-auth.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import TheHeader from '@/components/TheHeader.vue';</script>
 <template>
+	<the-header class="sticky top-0 right-0 left-0"/>
 	<main
 		class="
 			dark:bg-gray-800 dark:text-white
